4b2820da8c75e8dcaced47f6686df015daca6b6c
katherinestout <kjs14@hood.edu>
2019-04-05 15:00:50 -0400

    about responsive

--- src/components/About.js
+++ src/components/About.js
@@ -6,28 +6,31 @@ class About extends Component {
   render() {
     
     return (
-      <div className="About" id="about">
-       <div>
+      <div className="About">
        
-      <h2>About Me</h2>
+      <h2 className="about-title">About Me</h2>
       <hr/>
-    
-        <div className="writingspace">
-        <p>In 2017 I graduated from Hood College, 
+
+      <div className="container-about">
+      <div className="about-box-1 about-box">
+      <p>In 2017 I graduated from Hood College, 
         with a degree in Communications and a Minor in Studio Art.
         During my college years, I discovered two of my greatest passions;
         photography and coding. From 2016 - present I have ran my own photography 
         business. 
         </p>
-
-        <p>
+      </div>
+      <div className="about-box-2 about-box">
+      <p>
         While I dappled in coding for a few years, I finally took the plunge to 
         advance my career in 2018 when I earned my Full Stack Certificate from 
         George Washington University (GW). At GW I found the same sense of reward and 
         creativity as I have always pursued in photography. 
         </p>
-        
-
+      </div>
+      </div>
+    
+        <div className="about-box">
         <p>Download my resume!</p>
         <a href ="https://drive.google.com/file/d/1wGM16dNAdwz4wTbyKs-NF4n3FRSIjccj/view?usp=sharing">
         <button type="button" className="btn btn">
@@ -39,7 +42,7 @@ class About extends Component {
         </a>
         </div>
         </div>
-      </div>
+
     );
   }
 }
