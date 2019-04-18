fbd476b0f01e4ecfdb1dec5528db61aca6d35b75
katherinestout <kjs14@hood.edu>
2019-04-18 17:03:58 -0400

    Updates

---
+++ precache-manifest.e9d4caead93257455346607cacd05b9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c91ca684a6d719f75623451233f0979e",
+    "url": "/KatStout_ReactPortfolio/static/media/doctorwho.c91ca684.png"
+  },
+  {
+    "revision": "05c1c1a4ac85acd822aa",
+    "url": "/KatStout_ReactPortfolio/static/css/main.9a979f23.chunk.css"
+  },
+  {
+    "revision": "29500a733beb40a90aed",
+    "url": "/KatStout_ReactPortfolio/static/js/1.29500a73.chunk.js"
+  },
+  {
+    "revision": "a75921e5909c02e9d4b8",
+    "url": "/KatStout_ReactPortfolio/static/js/runtime~main.a75921e5.js"
+  },
+  {
+    "revision": "dffde38ae4140c3012efe981a3958588",
+    "url": "/KatStout_ReactPortfolio/static/media/clevercap.dffde38a.png"
+  },
+  {
+    "revision": "05c1c1a4ac85acd822aa",
+    "url": "/KatStout_ReactPortfolio/static/js/main.05c1c1a4.chunk.js"
+  },
+  {
+    "revision": "c64e06772a15fdeb52968ffca16a5172",
+    "url": "/KatStout_ReactPortfolio/static/media/drinkerator.c64e0677.png"
+  },
+  {
+    "revision": "813cbe469762aa45d086ba66a924d40b",
+    "url": "/KatStout_ReactPortfolio/static/media/photography.813cbe46.png"
+  },
+  {
+    "revision": "25b5344772c03c21fd89df48cd0803de",
+    "url": "/KatStout_ReactPortfolio/static/media/wordguess.25b53447.png"
+  },
+  {
+    "revision": "513a0f34871e2c18a95f6de1e3923286",
+    "url": "/KatStout_ReactPortfolio/static/media/portfolio.513a0f34.png"
+  },
+  {
+    "revision": "9f31eacffb2d584bb7e579b1f3628369",
+    "url": "/KatStout_ReactPortfolio/index.html"
+  }
+];
