fea45f9d7788cc3e13bbf2973222da8cc6e900ba
katherinestout <kjs14@hood.edu>
2019-05-01 12:23:00 -0400

    list

--- src/components/PortfolioPeices.js
+++ src/components/PortfolioPeices.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import portfolioList from './portfolio.json';
-import PortfolioCard from './PortfolioCard';
+import portfolioList from './portfolioSection/portfolio.json';
+import myPortfolio from './portfolioSection/myportfolio.json';
+import PortfolioCard from './portfolioSection/PortfolioCard';
+import MyPortfolioCard from './portfolioSection/MyPortfolioCard';
 import  "./style/portfolio.css";
 
 
 
 class Portfolio extends Component {
     state = {
-        projects: portfolioList
+        projects: portfolioList,
+        projectProject: myPortfolio
     };
 
     render () {
 
         return(
             
-            <div className="Portfolio" id="portfolio">
+    <div className="Portfolio" id="portfolio">
       <h2 className="title-box-portfolio"> 
       Portfolio Projects  
       </h2> <hr/>
@@ -32,6 +35,20 @@ class Portfolio extends Component {
                         img = {project.img}
                     />
                 ))}
+
+            {this.state.projectProject.map(myProject => (
+                 <MyPortfolioCard
+                 id={myProject.id}
+                 key={myProject.id}
+                 name={myProject.name}
+                 deploy={myProject.deploy}
+                 github={myProject.github}
+                 description={myProject.description}
+                 tools ={myProject.tools}
+                 img = {myProject.img}
+                 />
+            ))}
+                   
             </div>
             </div>
     
---
+++ src/components/portfolioSection/MyPortfolioCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './../style/card.css';
+
+const MyPortfolioCard = props => (
+
+
+  <div className="card-container">
+
+  <div className="img-box">
+  <img alt="true" src={props.img}/>
+  </div>
+
+
+  <div className="box-description">
+  <div className="box-description-box">
+  <h4>{props.name}</h4>
+  <ul>
+  <li>{props.description}</li>
+  <li>{props.tools}</li>
+  <li><a href = {props.github}> GitHub / Code</a></li>
+  </ul>
+  </div>
+  </div>
+
+
+  </div>
+
+
+
+)
+
+export default MyPortfolioCard;
