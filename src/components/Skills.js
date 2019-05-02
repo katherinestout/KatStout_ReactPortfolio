3aa2f4c586bdbb1e6589dace59c0581470d532bd
katherinestout <kjs14@hood.edu>
2019-05-02 11:32:58 -0400

    contact

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 import "./style/skills.css";
 import SkillsCard from './skills/SkillsCard';
 import skills from './skills/skills.json';
-import skillstwo from './skills/skillstwo.json';
-import skillsthree from './skills/skillsthree.json';
 
 
 class Skills extends Component {
 
     state = {
-        skillList: skills,
-        skillListTw: skillstwo,
-        skillListTh: skillsthree
+        skillList: skills
     };
-
-
 
   render() {
   
- 
-
-console.log(skills);
 
     return (
       <div className="Skills" id="skills">
-
       <h2 className="title-box-skill">Technical Skills</h2>
       <hr/>
       <div className="skills-wrapper">
 
             <div className="container-skills">
-
             <div className="skill-box">
             <ul className="skill-list">
             
@@ -51,33 +40,25 @@ console.log(skills);
 
             <div className="skill-box">
             <ul className="skill-list">
-            {this.state.skillListTw.map(skillstwo=>(
+            {this.state.skillList.map(skills=>(
        
        <SkillsCard 
-           id={skillstwo.id}
-           key={skillstwo.skillz}
-           skillz = {skillstwo.skillz}
-
+           id={skills.id}
+           key={skills.skillz2}
+           skillz = {skills.skillz2}
        />
-      
    ))}
             </ul>
-              </div>
-
-
-
+            </div>
             <div className="skill-box">
             <ul className="skill-list">
-            {this.state.skillListTh.map(skillsthree=>(
+            {this.state.skillList.map(skills=>(
        
        <SkillsCard 
-           id={skillsthree.id}
-           key={skillsthree.skillz}
-           skillz = {skillsthree.skillz}
-
+           id={skills.id}
+           key={skills.skillz3}
+           skillz = {skills.skillz3}
        />
-
-      
    ))}
              </ul>
             </div>
